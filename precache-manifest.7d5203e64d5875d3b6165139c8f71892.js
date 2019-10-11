8b9c4ae48e8d5b8ba2b63f361e004cc77fc59cae
Tammivuori Marko <s1900571@edu.bc.fi>
2019-10-11 10:18:54 +0300

    Updates

---
+++ precache-manifest.7d5203e64d5875d3b6165139c8f71892.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61d63d21e60291b5fa34ead6305baf2",
+    "url": "/helloworld/index.html"
+  },
+  {
+    "revision": "1a14612985342bd66df7",
+    "url": "/helloworld/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fae43c75c6bdf5af2874",
+    "url": "/helloworld/static/js/2.d07e09c2.chunk.js"
+  },
+  {
+    "revision": "1a14612985342bd66df7",
+    "url": "/helloworld/static/js/main.7d05e75d.chunk.js"
+  },
+  {
+    "revision": "16b289186cf529dcdef6",
+    "url": "/helloworld/static/js/runtime-main.4a206114.js"
+  }
+]);
